4eeaae6685ce6fbdea4fe524ae1bef7fd7569450
AlbertZhou <albertzhou@live.com>
2022-04-22 09:15:15 +0800

    create numbers.py

---
+++ Programs/python/ms-python-getting-started/numbers.py
@@ -0,0 +1,19 @@
+# + addition
+# - substraction
+# * Multiplication
+# / Division
+# ** Exponent
+
+# print('1~20的平方')
+# for num in range(1, 21):
+#     output = f'{num}, {num**2}'
+#     print(output)
+
+# print('1~10的立方')
+# for num in range(1, 11):
+#     output = f'{num}, {num**3}'
+#     print(output)
+
+days_in_feb = 28
+print(str(days_in_feb) + ' days in February')
+print(f'{days_in_feb} days in February')
